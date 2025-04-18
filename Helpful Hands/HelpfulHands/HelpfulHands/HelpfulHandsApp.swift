acf66fb931180fb67d97004d4291a6f9b77c9484
racheldandrea <rachbeth03@gmail.com>
2025-04-17 21:30:28 -0500

    Add files via upload

---
+++ Helpful Hands/HelpfulHands/HelpfulHands/HelpfulHandsApp.swift
@@ -0,0 +1,17 @@
+//
+//  HelpfulHandsApp.swift
+//  HelpfulHands
+//
+//  Created by Rachel D'Andrea on 4/17/25.
+//
+
+import SwiftUI
+
+@main
+struct HelpfulHandsApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
